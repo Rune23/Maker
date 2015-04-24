ad12b9fba6f5c92b97ae392915722a5f76b1502c
Joel Torstensson <oed3933@gmail.com>
2015-04-24 14:02:41 +0200

    Fixed indenting for solidity code.

--- depositableTestCoin.sol
+++ depositableTestCoin.sol
@@ -1,28 +1,28 @@
 contract DepositableContract {
-	function contractDepositor(address sender, uint amount) returns(bool success) {}
+    function contractDepositor(address sender, uint amount) returns(bool success) {}
 }
 
 contract TestCoin {
-	mapping (address => uint) balance;
-	function coinTransfer(address receiver, uint amount) {
-            if (balance[msg.sender] >= amount) {
-                  balance[msg.sender] -= amount;
-                  balance[receiver] += amount;
+    mapping (address => uint) balance;
+    function coinTransfer(address receiver, uint amount) {
+        if (balance[msg.sender] >= amount) {
+            balance[msg.sender] -= amount;
+            balance[receiver] += amount;
+        }
+    }
+    function depositToContract (address receiver, uint amount) {
+        DepositableContract cont;
+        cont = DepositableContract(receiver);
+        if (balance[msg.sender] >= amount) {
+            bool succ;
+            succ = cont.contractDepositor(msg.sender,amount);
+            if (succ) {
+                balance[msg.sender] -= amount;
+                balance[receiver] += amount;
             }
-      }
-	function depositToContract (address receiver, uint amount) {
-		DepositableContract cont;
-		cont = DepositableContract(receiver);
-		if (balance[msg.sender] >= amount) {
-			bool succ;
-			succ = cont.contractDepositor(msg.sender,amount);
-			if (succ) {
-				balance[msg.sender] -= amount;
-				balance[receiver] += amount;
-			}
-		}
-	}
-	function TestCoin() {
-		balance[msg.sender] = 1000;
-	}
+        }
+    }
+    function TestCoin() {
+        balance[msg.sender] = 1000;
+    }
 }
--- depositableTestContract.sol
+++ depositableTestContract.sol
@@ -1,26 +1,26 @@
 contract DepositorCoin {
-	function sendCoin(address receiver, uint amount) {
-      }
+    function sendCoin(address receiver, uint amount) {
+    }
 }
 
 contract DepositableContract {
-	address depositorCoinAddr;
-	mapping (address => uint) balance;
-	function contractDepositor(address sender, uint amount) returns(bool success) {
-		if (msg.sender == depositorCoinAddr) {
-			balance[sender] += amount;
-			return true;
-		}
-	}
-	function withdrawCoin(address receiver, uint amount) {
-		if (balance[msg.sender] >= amount) {
-			DepositorCoin coinInstance;
-			coinInstance = DepositorCoin(depositorCoinAddr);
-			coinInstance.coinTransfer(receiver, amount);
-			balance[msg.sender] -= amount;
-		}
-	}
-	function DepositableContract() {
-		depositedCoinAddr = 0x5d7010d9955e5eeebaf4405584a86ca684c56d11;
-	}
+    address depositorCoinAddr;
+    mapping (address => uint) balance;
+    function contractDepositor(address sender, uint amount) returns(bool success) {
+        if (msg.sender == depositorCoinAddr) {
+            balance[sender] += amount;
+            return true;
+        }
+    }
+    function withdrawCoin(address receiver, uint amount) {
+        if (balance[msg.sender] >= amount) {
+            DepositorCoin coinInstance;
+            coinInstance = DepositorCoin(depositorCoinAddr);
+            coinInstance.coinTransfer(receiver, amount);
+            balance[msg.sender] -= amount;
+        }
+    }
+    function DepositableContract() {
+        depositedCoinAddr = 0x5d7010d9955e5eeebaf4405584a86ca684c56d11;
+    }
 }
